8fc92cc4db5f880b8b2cd3e9a35c071dd0e86ddc
tim24ktk <tim24ktk@mail.ru>
2024-01-03 02:05:36 +0300

    desctop vertion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 var swiper = new Swiper(".container", {
+  autoplay: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView : 1,
+    },
+
+    1024: {
+      slidesPerView : 2,
+    },
+  },  
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 50,
+    depth: 350,
+  },
+
+});
+
+var swiper_review = new Swiper(".review__wrapper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
@@ -10,7 +36,7 @@ var swiper = new Swiper(".container", {
     slideShadows: true,
   },
   autoplay: {
-    delay: 2500,
+    delay: 10000,
     disableOnInteraction: false,
   },
   loop: true,
